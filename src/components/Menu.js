58527990beb12a4a673325eb5f70a50e32a1d856
Luis Daniel <gonzalez.zamudio.luis.daniel@gmail.com>
2023-07-04 00:56:59 -0600

    feat: se creó el proyecto, se instalaron dependencias, se hizo el carpetado, agregó la vista principal, styles, las secciones about me, y personal information, además de links, se comezó el componente menu y se implementó en la vista principal, unicamente usan anchors

---
+++ src/components/Menu.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+const Menu = () => {
+    const [activeLink, setActiveLink] = useState('');
+
+    const handleLinkClick = (link) => {
+        setActiveLink(link);
+    };
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a
+                        href="#section1"
+                        className={activeLink === 'section1' ? 'active' : ''}
+                        onClick={() => handleLinkClick('section1')}
+                    >
+                        Section 1
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="#section2"
+                        className={activeLink === 'section2' ? 'active' : ''}
+                        onClick={() => handleLinkClick('section2')}
+                    >
+                        Section 2
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="#section3"
+                        className={activeLink === 'section3' ? 'active' : ''}
+                        onClick={() => handleLinkClick('section3')}
+                    >
+                        Section 3
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Menu;
---
+++ src/pages/Home.js
@@ -0,0 +1,143 @@
+import '../styles/styles.scss';
+import Menu from '../components/Menu'
+// import Typography from '@mui/material/Typography';
+export default function Home(){
+    return (
+        <>
+            <header>
+                <Menu />
+            </header>
+            <main>
+                <section id="section1">
+                    <Profile />
+                </section>
+                <section id="section2">
+                    <AboutMe />
+                </section>
+            </main>
+        </>
+    )
+}
+
+function AboutMe() {
+    return (
+        <>
+            <section className="about-me-section">
+                <h1> About Me </h1>
+                <p>
+                    I am passionate about web development and have experience building web applications,
+                    I have a strong understanding of programming languages like HTML, CSS, and JavaScript,
+                    as well as experience using popular frameworks like Vue.js.
+
+                    I have teamwork skills, which allow me to collaborate effectively with designers,
+                    developers, and other professionals to achieve project goals.
+
+                    I am looking for a web development position where I can apply my knowledge,
+                    skills and passion for technology to contribute to the development of scalable,
+                    innovative and successful web systems.
+                    I am passionate about web development and have experience building web applications,
+                    I have a strong understanding of programming languages like HTML, CSS, and JavaScript,
+                    as well as experience using popular frameworks like Vue.js.
+
+                    I have teamwork skills, which allow me to collaborate effectively with designers,
+                    developers, and other professionals to achieve project goals.
+
+                    I am looking for a web development position where I can apply my knowledge,
+                    skills and passion for technology to contribute to the development of scalable,
+                    innovative and successful web systems.
+                    I am passionate about web development and have experience building web applications,
+                    I have a strong understanding of programming languages like HTML, CSS, and JavaScript,
+                    as well as experience using popular frameworks like Vue.js.
+
+                    I have teamwork skills, which allow me to collaborate effectively with designers,
+                    developers, and other professionals to achieve project goals.
+
+                    I am looking for a web development position where I can apply my knowledge,
+                    skills and passion for technology to contribute to the development of scalable,
+                    innovative and successful web systems.
+                    I am passionate about web development and have experience building web applications,
+                    I have a strong understanding of programming languages like HTML, CSS, and JavaScript,
+                    as well as experience using popular frameworks like Vue.js.
+
+                    I have teamwork skills, which allow me to collaborate effectively with designers,
+                    developers, and other professionals to achieve project goals.
+
+                    I am looking for a web development position where I can apply my knowledge,
+                    skills and passion for technology to contribute to the development of scalable,
+                    innovative and successful web systems.
+                    I am passionate about web development and have experience building web applications,
+                    I have a strong understanding of programming languages like HTML, CSS, and JavaScript,
+                    as well as experience using popular frameworks like Vue.js.
+
+                    I have teamwork skills, which allow me to collaborate effectively with designers,
+                    developers, and other professionals to achieve project goals.
+
+                    I am looking for a web development position where I can apply my knowledge,
+                    skills and passion for technology to contribute to the development of scalable,
+                    innovative and successful web systems.
+                </p>
+            </section>
+        </>
+    )
+}
+
+function Profile() {
+    return (
+        <>
+            <section className="profile-me-section">
+                <h1>Luis Daniel González Zamudio</h1>
+                <h3>Graduated from Computer Systems Engineering with experience in web development</h3>
+                <h4>Links</h4>
+                <p>
+                    I am passionate about web development and have experience building web applications,
+                    I have a strong understanding of programming languages like HTML, CSS, and JavaScript,
+                    as well as experience using popular frameworks like Vue.js.
+
+                    I have teamwork skills, which allow me to collaborate effectively with designers,
+                    developers, and other professionals to achieve project goals.
+
+                    I am looking for a web development position where I can apply my knowledge,
+                    skills and passion for technology to contribute to the development of scalable,
+                    innovative and successful web systems.
+                    I am passionate about web development and have experience building web applications,
+                    I have a strong understanding of programming languages like HTML, CSS, and JavaScript,
+                    as well as experience using popular frameworks like Vue.js.
+
+                    I have teamwork skills, which allow me to collaborate effectively with designers,
+                    developers, and other professionals to achieve project goals.
+
+                    I am looking for a web development position where I can apply my knowledge,
+                    skills and passion for technology to contribute to the development of scalable,
+                    innovative and successful web systems.
+                    I am passionate about web development and have experience building web applications,
+                    I have a strong understanding of programming languages like HTML, CSS, and JavaScript,
+                    as well as experience using popular frameworks like Vue.js.
+
+                    I have teamwork skills, which allow me to collaborate effectively with designers,
+                    developers, and other professionals to achieve project goals.
+
+                    I am looking for a web development position where I can apply my knowledge,
+                    skills and passion for technology to contribute to the development of scalable,
+                    innovative and successful web systems.
+                    I am passionate about web development and have experience building web applications,
+                    I have a strong understanding of programming languages like HTML, CSS, and JavaScript,
+                    as well as experience using popular frameworks like Vue.js.
+
+                    I have teamwork skills, which allow me to collaborate effectively with designers,
+                    developers, and other professionals to achieve project goals.
+
+                    I am looking for a web development position where I can apply my knowledge,
+                    skills and passion for technology to contribute to the development of scalable,
+                    innovative and successful web systems.
+
+                </p>
+                <a href="https://www.linkedin.com/in/luis-daniel-gonzalez-zamudio/" target="_blank" rel="noreferrer">
+                    <img src="assets/icons/linkedin.png" alt=""/>
+                </a>
+                <a href="https://github.com/DanielGlzZ" target="_blank" rel="noreferrer">
+                    <img src="assets/icons/github.png" alt=""/>
+                </a>
+            </section>
+        </>
+    )
+}
